c5bba157db3033b3b7d0fb6b82d3201ed995a10b
Andreas Signer <asigner@gmail.com>
2021-02-14 14:50:16 +0100

    Added copyright headers

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2021 Andreas Signer <asigner@gmail.com>
+ *
+ * This file is part of cbm-font-editor.
+ *
+ * cbm-font-editor is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * cbm-font-editor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with cbm-font-editor.  If not, see <http://www.gnu.org/licenses/>.
+ */
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
