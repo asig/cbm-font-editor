ef39beff2d832ab6080ad6d382646bfc61057582
Andreas Signer <asigner@gmail.com>
2021-02-14 01:10:23 +0100

    Add a welcome dialog.

--- src/App.js
+++ src/App.js
@@ -12,14 +12,66 @@ import globals from "./globals"
 import './App.css';
 
 import Content from "./Content";
+import WelcomeDialog from "./WelcomeDialog";
+import ErrorMsg from "./ErrorMsg";
+import SaveDialog from "./SaveDialog";
 
 const theme = createMuiTheme({
     overrides: {
+        MuiDivider: {
+            root: {
+                backgroundColor: globals.colors.fg,
+            }
+        },
         MuiContainer: {
             root: {
                 backgroundColor: globals.colors.bg,
-            },
+            }
         },
+        MuiCard: {
+            root: {
+                height: "100%"
+            }
+        },
+        MuiCardHeader: {
+            root: {
+                backgroundColor: globals.colors.brown,
+                color: globals.colors.white,
+                padding: 0,
+            },
+            title: {
+                fontFamily: "Microgramma-D-Medium",
+                // fontFamily: "Comfortaa",
+                fontSize: "1.2rem",
+            }
+        },
+        MuiCardContent: {
+            root: {
+                display: 'flex',
+                justifyContent: 'center',
+            }
+        },
+        MuiButton: {
+            root: {
+                fontFamily: "Comfortaa",
+                // fontFamily: "Microgramma-D-Medium",
+                textTransform: "unset",
+                minWidth: "0px"
+            },
+            contained: {
+                color: globals.colors.white,
+                backgroundColor: globals.colors.brown
+            }
+        },
+        MuiPaper: {
+            root: {
+                border: "1px solid " + globals.colors.brown,
+                backgroundColor: globals.colors.bg,
+//                backgroundColor: globals.colors.white,
+                textAlign: 'center',
+                // height: "100%",
+            }
+        }
     }
 });
 
@@ -28,30 +80,34 @@ class App extends React.Component {
         super(props);
 
         this.render = this.render.bind(this)
-    }
 
+        this.errorMessageRef = React.createRef()
+        this.saveDialogRef = React.createRef()
+    }
 
     render() {
         return (
             <ThemeProvider theme={theme}>
-                <Container  >
+                <Container>
                     <Grid container justify="center" alignItems="flex-end" spacing={3}>
                         <Grid item xs={12}>
                             <img src={Logo} width={"100%"} alt={"CBM Font Editor"}/>
                         </Grid>
                         <Grid item xs={12}>
-                            <Content/>
+                            <Content errorMessageRef={this.errorMessageRef} saveDialogRef={this.saveDialogRef}/>
                         </Grid>
                     </Grid>
                 </Container>
                 <footer>
                     © 2021 Andreas Signer
                 </footer>
+                <ErrorMsg ref={this.errorMessageRef}/>
+                <WelcomeDialog open={true}/>
+                <SaveDialog ref={this.saveDialogRef} />
             </ThemeProvider>
         );
     }
 }
-
 
 export default App;
 
---
+++ src/WelcomeDialog.js
@@ -0,0 +1,108 @@
+import React from 'react';
+
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogActions from "@material-ui/core/DialogActions";
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import InputLabel from '@material-ui/core/InputLabel';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import Divider from '@material-ui/core/Divider';
+import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
+import Select from '@material-ui/core/Select';
+import globals from "./globals";
+
+class WelcomeDialog extends React.Component {
+
+    static defaultState = {
+        open: false,
+        dontShow: false,
+    }
+
+    static dontShowKey = "dontShowWelcomeVersion"
+    static dontShowVersion = "0"
+
+    constructor(props) {
+        super(props);
+
+        // Only show the dialog if the user did not opt out.
+        var v = localStorage.getItem(WelcomeDialog.dontShowKey)
+        const dontShow = v === WelcomeDialog.dontShowVersion
+        this.state = {
+            open: !dontShow,
+            dontShow: dontShow,
+        }
+
+        this.handleClose = this.handleClose.bind(this)
+        this.handleDontShow = this.handleDontShow.bind(this)
+        this.render = this.render.bind(this)
+    }
+
+    showDialog() {
+    }
+
+    handleClose(evt, reason) {
+        console.log(evt, reason)
+        if (reason === 'clickaway') {
+            return;
+        }
+        this.setState({open: false});
+    }
+
+    handleDontShow(evt) {
+        const checked = evt.target.checked
+        if (checked) {
+            localStorage.setItem(WelcomeDialog.dontShowKey, WelcomeDialog.dontShowVersion)
+        } else {
+            localStorage.removeItem(WelcomeDialog.dontShowKey)
+        }
+        this.setState({dontShow: checked});
+    }
+
+    render() {
+        return <Dialog open={this.state.open} onClose={this.handleClose}>
+            <DialogTitle>Welcome to {globals.product.name} v{globals.product.version}!</DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                    <p>
+                        With this little tool, you can modify fonts for a wide range of Commodore home computers, and
+                        download your creations as binary files (in plain and 'prg' format), as BASIC programs or
+                        as source code for the cbmasm cross-assembler.
+                    </p>
+                    <p>
+                        To get you started, you need a 2k Commodore font in binary format, which you can get for example
+                        from <a href="http://www.zimmers.net/anonftp/pub/cbm/firmware/characters/" target="_blank">zimmers.net</a>.
+                    </p>
+                    <p>
+                        This is an early version with basic functionality. Most notably, multi-color fonts are not
+                        supported yet.
+                    </p>
+                    <p>
+                        And last but not least, this app completely runs in your browser. All your data is kept on your
+                        own machine, nothing is sent over the internets!
+                    </p>
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <FormGroup row>
+                    <FormControlLabel
+                        control={<Checkbox checked={this.state.dontShow} onChange={this.handleDontShow} name={"dontShow"}/>}
+                        label="Don't show this again"
+                    />
+                    <Button variant="contained" onClick={this.handleClose}>
+                        Close
+                    </Button>
+                </FormGroup>
+            </DialogActions>
+        </Dialog>
+    }
+}
+
+export default WelcomeDialog;
