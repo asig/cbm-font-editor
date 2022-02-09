5a92dd0bafbe7ce53296f81a92ea9ca18ba91d60
Andreas Signer <asigner@gmail.com>
2022-02-09 09:18:42 +0100

    Added ROM fonts

--- src/Content.js
+++ src/Content.js
@@ -29,7 +29,9 @@ import Card from "@material-ui/core/Card"
 import CardHeader from "@material-ui/core/CardHeader"
 import CardContent from "@material-ui/core/CardContent"
 import FormControlLabel from '@material-ui/core/FormControlLabel'
-import Checkbox from '@material-ui/core/Checkbox';
+import Checkbox from '@material-ui/core/Checkbox'
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem'
 
 import Char from "./model/Char"
 import Font from "./model/Font"
@@ -39,13 +41,26 @@ import DisableableButton from "./DisableableButton"
 import CharEditField from "./CharEditField"
 import DirectionTable from "./DirectionTable"
 import FontView from "./FontView"
-import RotationTable from "./RotationTable";
-import ColorSelector from "./ColorSelector";
+import RotationTable from "./RotationTable"
+import ColorSelector from "./ColorSelector"
 import Utils from "./Utils"
 
 import palette from "./palette"
 
-import './App.css';
+import './App.css'
+import EmbeddedFonts from "./EmbeddedFonts"
+
+import './FontMenuItem.css'
+
+function FontMenuItem(props) {
+    if (props.isSeparator) {
+        return <hr class={"FontMenuItem"}/>
+    }
+    return (
+        <MenuItem className={["FontMenuItem", props.classNames]} onClick={props.onClick}>{props.children}</MenuItem>
+    );
+}
+
 
 class Content extends React.Component {
 
@@ -53,6 +68,7 @@ class Content extends React.Component {
         super(props);
 
         this.render = this.render.bind(this)
+        this.loadFontFromData = this.loadFontFromData.bind(this)
         this.loadFont = this.loadFont.bind(this)
         this.modifyChar = this.modifyChar.bind(this)
         this.selectChar = this.selectChar.bind(this)
@@ -88,6 +104,9 @@ class Content extends React.Component {
         this.showHelp = this.showHelp.bind(this)
         this.showAbout = this.showAbout.bind(this)
 
+        this.showLoadMenu = this.showLoadMenu.bind(this)
+        this.closeLoadMenu = this.closeLoadMenu.bind(this)
+
         this.charEditFieldRef = React.createRef();
         this.fontViewRef = React.createRef();
         this.fileInputRef = React.createRef()
@@ -100,6 +119,7 @@ class Content extends React.Component {
             selectedColor: 3,
             multicol: false,
             cols: [palette[1], palette[11], palette[13], palette[0]],
+            loadMenuAnchor: null,
         }
         this.font = new Font(null);
 
@@ -208,7 +228,9 @@ class Content extends React.Component {
                 }
             }
         }
-        return this.parentKeyHandler(evt)
+        if (this.parentKeyHandler) {
+            return this.parentKeyHandler(evt)
+        }
     }
 
     applyHistoryState(s) {
@@ -349,11 +371,33 @@ class Content extends React.Component {
     }
 
     showLoadFontDlg() {
+        this.closeLoadMenu()
         this.fileInputRef.current.click()
     }
 
     showSaveFontDlg() {
         this.props.saveDialogRef.current.showDialog(this.font)
+    }
+
+    loadFontFromData(data) {
+        if (data.byteLength === 2050) {
+            // binary file with load address at ofs 0
+            data = data.slice(2)
+        }
+
+        const fv = this.fontViewRef.current
+        const cef = this.charEditFieldRef.current
+
+        // Set new font
+        const chars = [...Array(256).keys()].map((i) => new Char(data.slice(i * 8, i * 8 + 8)))
+        this.font = new Font(chars)
+        fv.setFont(this.font)
+
+        // Set selected char
+        const ch = this.font.getChar(this.state.selectedChar)
+        cef.setChar(ch)
+
+        this.resetHistory({selected: this.state.selectedChar, ch: ch})
     }
 
     loadFont(f) {
@@ -369,29 +413,20 @@ class Content extends React.Component {
                 return
             }
             var data = new Uint8Array(e.target.result)
-            if (len === 2050) {
-                // binary file with load address at ofs 0
-                data = data.slice(2)
-            }
-
-            const fv = this.fontViewRef.current
-            const cef = this.charEditFieldRef.current
-
-            // Set new font
-            const chars = [...Array(256).keys()].map((i) => new Char(data.slice(i * 8, i * 8 + 8)))
-            this.font = new Font(chars)
-            fv.setFont(this.font)
-
-            // Set selected char
-            const ch = this.font.getChar(this.state.selectedChar)
-            cef.setChar(ch)
-
-            this.resetHistory({selected: this.state.selectedChar, ch: ch})
+            this.loadFontFromData(data);
         };
         reader.onerror = (e) => {
             this.props.errorMessageRef.current.showError("Error while loading file: " + e)
         }
         reader.readAsArrayBuffer(f);
+    }
+
+    showLoadMenu(evt) {
+        this.setState({loadMenuAnchor: evt.currentTarget})
+    }
+
+    closeLoadMenu() {
+        this.setState({loadMenuAnchor: null})
     }
 
     selectColor(idx) {
@@ -453,6 +488,25 @@ class Content extends React.Component {
                                     />
                                 </Box>
                                 <Box display="flex" flexDirection="column">
+                                    <DisableableButton
+                                        ref={this.loadBtnRef}
+                                        style={{marginBottom: "1rem", marginLeft: "1rem"}}
+                                        variant="contained"
+                                        onClick={this.showLoadMenu}>
+                                        Std Font
+                                    </DisableableButton>
+                                    <Menu
+                                        anchorEl={this.state.loadMenuAnchor}
+                                        keepMounted
+                                        open={Boolean(this.state.loadMenuAnchor)}
+                                        onClose={this.closeLoadMenu}
+                                    >
+                                        {/*<MenuItem onClick={this.showLoadFontDlg}>Load file...</MenuItem>*/}
+                                        {/*<hr/>*/}
+                                        {EmbeddedFonts.map((font) => (
+                                            <FontMenuItem isSeparator={font.name === ""} onClick={() => {this.loadFontFromData(font.data); this.closeLoadMenu()}}>{font.name}</FontMenuItem>
+                                        ))}
+                                    </Menu>
                                     <Box>
                                         {/* See https://stackoverflow.com/questions/8350927/file-upload-button-without-input-field for details */}
                                         <input
